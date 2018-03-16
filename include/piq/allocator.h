ecd5130eea4e59e9271949c44b970ce0b7f9202e
Yuri Kilochek <yuri.kilochek@gmail.com>
2018-03-16 20:27:48 +0300

    Use explicitly sized integers in allocation flags.

--- include/piq/allocator.h
+++ include/piq/allocator.h
@@ -1,15 +1,16 @@
 #ifndef PIQ_IMPL_INCLUDE_GUARD_ALLOCATOR
 #define PIQ_IMPL_INCLUDE_GUARD_ALLOCATOR
 
+#include <stdint.h>
 #include <stddef.h>
 
 #if __cplusplus
 extern "C" {
 #endif
 
-typedef unsigned long int piq_allocation_flags_t;
+typedef uint_least32_t piq_allocation_flags_t;
 
-#define PIQ_FILLED_WITH_ZEROS (0x00000001ul)
+#define PIQ_FILLED_WITH_ZEROS (UINT32_C(0x00000001))
 
 typedef struct piq_impl_allocator piq_allocator_t;
 
@@ -40,16 +41,12 @@ void piq_release_allocator(piq_allocator_t *allocator) {
 static inline
 void *piq_allocate(piq_allocator_t *allocator, ptrdiff_t *size,
                    ptrdiff_t alignment, piq_allocation_flags_t flags)
-{
-    return allocator->ops->allocate(allocator, size, alignment, flags);
-}
+{ return allocator->ops->allocate(allocator, size, alignment, flags); }
 
 static inline
 void piq_deallocate(piq_allocator_t *allocator, void *ptr, ptrdiff_t size,
                     ptrdiff_t alignment)
-{
-    allocator->ops->deallocate(allocator, ptr, size, alignment);
-}
+{ allocator->ops->deallocate(allocator, ptr, size, alignment); }
 
 extern
 piq_allocator_t *const piq_default_allocator;
