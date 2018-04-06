5c502a4908bb0f84de5618366d3ee2ad644177d0
Yuri Kilochek <yuri.kilochek@gmail.com>
2018-04-06 16:02:02 +0300

    Split reallocation.

--- include/piq/allocator.h
+++ include/piq/allocator.h
@@ -1,25 +1,22 @@
 #ifndef PIQ_IMPL_INCLUDE_GUARD_ALLOCATOR
 #define PIQ_IMPL_INCLUDE_GUARD_ALLOCATOR
 
-#include <stdint.h>
 #include <stddef.h>
 
 #if __cplusplus
 extern "C" {
 #endif
 
-typedef uint_least32_t piq_allocation_flags_t;
+typedef unsigned int piq_allocation_flags_t;
 
-#define PIQ_FILLED_WITH_ZEROS (UINT32_C(0x00000001))
+#define PIQ_FILLED_WITH_ZEROS 0x0001u
+
 
 typedef struct piq_impl_allocator piq_allocator_t;
 
 typedef struct piq_impl_allocator_ops {
-    void (*retain)(piq_allocator_t *allocator);
-    void (*release)(piq_allocator_t *allocator);
-
     void *(*allocate)(piq_allocator_t *allocator, ptrdiff_t *size,
-                      ptrdiff_t alignment, piq_allocation_flags_t flags);
+                      ptrdiff_t alignment, piq_allocation_flags_t *flags);
     void (*deallocate)(piq_allocator_t *allocator, void *ptr, ptrdiff_t size,
                        ptrdiff_t alignment);
 } piq_allocator_ops_t;
@@ -29,24 +26,15 @@ struct piq_impl_allocator {
 };
 
 static inline
-void piq_retain_allocator(piq_allocator_t *allocator) {
-    allocator->ops->retain(allocator);
-}
-
-static inline
-void piq_release_allocator(piq_allocator_t *allocator) {
-    allocator->ops->release(allocator);
-}
-
-static inline
 void *piq_allocate(piq_allocator_t *allocator, ptrdiff_t *size,
-                   ptrdiff_t alignment, piq_allocation_flags_t flags)
+                   ptrdiff_t alignment, piq_allocation_flags_t *flags)
 { return allocator->ops->allocate(allocator, size, alignment, flags); }
 
 static inline
 void piq_deallocate(piq_allocator_t *allocator, void *ptr, ptrdiff_t size,
                     ptrdiff_t alignment)
 { allocator->ops->deallocate(allocator, ptr, size, alignment); }
+
 
 extern
 piq_allocator_t *const piq_default_allocator;
--- src/allocator.c
+++ src/allocator.c
@@ -4,22 +4,12 @@
 #include <string.h>
 
 static
-void default_retain(piq_allocator_t *base) {
-    (void)base;
-}
-
-static
-void default_release(piq_allocator_t *base) {
-    (void)base;
-}
-
-static
 void *default_allocate(piq_allocator_t *base, ptrdiff_t *size,
-                       ptrdiff_t alignment, piq_allocation_flags_t flags)
+                       ptrdiff_t alignment, piq_allocation_flags_t *flags)
 {
     (void)base;
 
-    _Bool filled_with_zeros = flags & PIQ_FILLED_WITH_ZEROS;
+    _Bool filled_with_zeros = flags && (*flags & PIQ_FILLED_WITH_ZEROS);
 
     void *ptr;
     if (filled_with_zeros && alignment <= (ptrdiff_t)_Alignof(max_align_t)) {
@@ -44,8 +34,6 @@ void default_deallocate(piq_allocator_t *base, void *ptr, ptrdiff_t size,
 
 piq_allocator_t *const piq_default_allocator = &(piq_allocator_t) {
     .ops = &(piq_allocator_ops_t) {
-        .retain = default_retain,
-        .release = default_release,
         .allocate = default_allocate,
         .deallocate = default_deallocate,
     },
