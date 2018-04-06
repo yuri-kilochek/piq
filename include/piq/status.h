9d98c9892051e156537a8d754e26dd61fc237e9f
Yuri Kilochek <yuri.kilochek@gmail.com>
2018-04-06 16:05:50 +0300

    Status is unsized integer.

--- include/piq/status.h
+++ include/piq/status.h
@@ -1,16 +1,14 @@
 #ifndef PIQ_IMPL_INCLUDE_GUARD_STATUS
 #define PIQ_IMPL_INCLUDE_GUARD_STATUS
 
-#include <stdint.h>
-
 #if __cplusplus
 extern "C" {
 #endif
 
-typedef int_least32_t piq_status_t;
+typedef unsigned int piq_status_t;
 
-#define PIQ_OK                (INT32_C(0))
-#define PIQ_ALLOCATION_FAILED (INT32_C(1))
+#define PIQ_OK                0u
+#define PIQ_ALLOCATION_FAILED 1u
 
 #if __cplusplus
 } // extern "C"
